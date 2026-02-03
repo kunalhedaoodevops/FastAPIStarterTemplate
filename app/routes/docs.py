f0f0b5ef6cf565485ca5c24cf339af8e3e59e885
kunalhedaoodevops <kunalhedaoodevops@gmail.com>
2026-02-03 21:46:21 +0530

    Add Scalar Configuration in the python code

--- app/routes/docs.py
+++ app/routes/docs.py
@@ -1,5 +1,7 @@
 from scalar_fastapi import get_scalar_api_reference
 from fastapi import APIRouter
+from scalar_fastapi import Theme
+
 
 router = APIRouter(prefix='/docs', tags=['Docs'])
 @router.get("/scalar", include_in_schema=True, summary="Scalar API Reference")
@@ -9,4 +11,17 @@ async def scalar_html():
         openapi_url="/openapi.json",
         # Avoid CORS issues (optional)
         scalar_proxy_url="https://proxy.scalar.com",
+
+        # ---- UI CONFIG ----
+        theme=Theme.DEEP_SPACE,
+        show_sidebar=True,
+        hide_client_button=False,
+        hide_search=False,
+        hide_dark_mode_toggle=False,
+        with_default_fonts=True,
+        default_open_all_tags=False,
+        expand_all_model_sections=False,
+        expand_all_responses=False,
+        order_required_properties_first=True,
+
     )
